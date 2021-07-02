a304ec512f5844a87820a0025ec4e5f458819075
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-07-02 14:46:10 +0200

    add tabs in editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
-  link ={
+  link = {
   isHomeActive : true,
   isAboutActive : false,
   isBackgroundActive : false,
   isSkillActive : false,
   isProjectsActive : false,
   isContactActive : false
-  }
+  };
+  tab = {
+    isHomeActive : true,
+    isAboutActive : false,
+    isBackgroundActive : false,
+    isSkillActive : false,
+    isProjectsActive : false,
+    isContactActive : false
+  };
 
+  // tslint:disable-next-line:typedef
   chooseDisplayed(element){
     // Object.entries(this.link).forEach((element) => this.link = )
     Object.keys(this.link).forEach(val => {
-      this.link[val] = false
+      this.link[val] = false;
     });
-    switch(element){
+    switch (element){
       case 'home':
-        this.link.isHomeActive =true;
+        this.link.isHomeActive = true;
+        this.tab.isHomeActive = true;
         break;
       case 'about':
-        this.link.isAboutActive =true;
+        this.link.isAboutActive = true;
+        this.tab.isAboutActive = true;
         break;
       case 'background':
-        this.link.isBackgroundActive =true;
+        this.link.isBackgroundActive = true;
+        this.tab.isBackgroundActive = true;
         break;
       case 'skills':
-        this.link.isSkillActive =true;
+        this.link.isSkillActive = true;
+        this.tab.isSkillActive = true;
         break;
       case 'projects':
-        this.link.isProjectsActive =true;
+        this.link.isProjectsActive = true;
+        this.tab.isProjectsActive = true;
         break;
       case 'contact':
-        this.link.isContactActive =true;
+        this.link.isContactActive = true;
+        this.tab.isContactActive = true;
         break;
 
     }
