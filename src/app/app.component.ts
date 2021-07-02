ccf1ec123d2380b7eb3c7c18c982704046c9e444
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-07-02 14:54:22 +0200

    add function closeTab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,36 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
   link = {
-  isHomeActive : true,
-  isAboutActive : false,
-  isBackgroundActive : false,
-  isSkillActive : false,
-  isProjectsActive : false,
-  isContactActive : false
+    isHomeActive: true,
+    isBackgroundActive: false,
+    isSkillActive: false,
+    isProjectsActive: false,
+    isContactActive: false
   };
   tab = {
-    isHomeActive : true,
-    isAboutActive : false,
-    isBackgroundActive : false,
-    isSkillActive : false,
-    isProjectsActive : false,
-    isContactActive : false
+    isHomeActive: true,
+    isBackgroundActive: false,
+    isSkillActive: false,
+    isProjectsActive: false,
+    isContactActive: false
   };
 
   // tslint:disable-next-line:typedef
-  chooseDisplayed(element){
+  chooseDisplayed(element) {
     // Object.entries(this.link).forEach((element) => this.link = )
     Object.keys(this.link).forEach(val => {
       this.link[val] = false;
     });
-    switch (element){
+    switch (element) {
       case 'home':
         this.link.isHomeActive = true;
         this.tab.isHomeActive = true;
-        break;
-      case 'about':
-        this.link.isAboutActive = true;
-        this.tab.isAboutActive = true;
         break;
       case 'background':
         this.link.isBackgroundActive = true;
@@ -55,9 +49,33 @@ export class AppComponent {
         this.link.isContactActive = true;
         this.tab.isContactActive = true;
         break;
-
     }
+  }
 
 
+  closeTab(element) {
+    switch (element) {
+      case 'home':
+        this.link.isHomeActive = false;
+        this.tab.isHomeActive = false;
+        break;
+
+      case 'background':
+        this.link.isBackgroundActive = false;
+        this.tab.isBackgroundActive = false;
+        break;
+      case 'skills':
+        this.link.isSkillActive = false;
+        this.tab.isSkillActive = false;
+        break;
+      case 'projects':
+        this.link.isProjectsActive = false;
+        this.tab.isProjectsActive = false;
+        break;
+      case 'contact':
+        this.link.isContactActive = false;
+        this.tab.isContactActive = false;
+        break;
+    }
   }
 }
