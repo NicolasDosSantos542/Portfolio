2400d960806f7b505ae4a071f5403a1077598cac
Nicolas <nicolas.de-meyere-dos-santos@epitech.eu>
2021-07-06 15:31:48 +0200

    start coding

--- src/app/component/console/console.component.ts
+++ src/app/component/console/console.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-console',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./console.component.css']
 })
 export class ConsoleComponent implements OnInit {
+  value = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
